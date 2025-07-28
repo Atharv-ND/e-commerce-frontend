6645508a83dfd0c5a0206fd3596209205f1826b9
Atharv-ND <atharvnaikdesai@gmail.com>
2025-07-28 23:03:45 +0530

    latest

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ✅ Prevent Vercel from failing due to lint
   },
+  images: {
+    domains: ['images.unsplash.com'],
+  },
 };
 
 module.exports = nextConfig;
